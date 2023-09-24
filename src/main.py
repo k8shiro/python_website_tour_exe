7e137dc4cd467f0437a516e2655b9e22e4f7cea3
k8shiro <koheiyatsushiro@yahoo.co.jp>
2023-09-24 19:43:42 +0900

    chromium_executable_path

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 from playwright.sync_api import sync_playwright
 
 def main():
+    chromium_executable_path = "C:\Program Files\Google\Chrome\Application\chrome.exe"
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(executable_path=chromium_executable_path, headless=False)
         page = browser.new_page()
         page.goto('https://playwright.dev/')
         page.wait_for_timeout(3000)
