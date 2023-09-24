00301e8493d5e081300b599f6fbcb11b94a4372b
k8shiro <koheiyatsushiro@yahoo.co.jp>
2023-09-24 19:25:08 +0900

    headless false

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from playwright.sync_api import sync_playwright
 
 def main():
     with sync_playwright() as p:
-        browser = p.chromium.launch({'channel': 'chrome'})
+        browser = p.chromium.launch(headless=False)
         page = browser.new_page()
         page.goto('https://playwright.dev/')
         page.wait_for_timeout(3000)
